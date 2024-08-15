5b4db6593a4521724746ab51793ceb53000d136f
AlexReyes54 <49156377+AlexitoReyes54@users.noreply.github.com>
2024-08-15 13:23:44 -0400

    Update BlogRedirectCta.tsx

--- components/blogs/BlogRedirectCta.tsx
+++ components/blogs/BlogRedirectCta.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-daisyui';
 import { useTranslation } from 'next-i18next';
 
 const BlogRedirectCta = () => {
-    const { t } = useTranslation("common");
+  const { t } = useTranslation('common');
 
   return (
     <div
@@ -12,15 +12,13 @@ const BlogRedirectCta = () => {
          flex-col border-2 border-base-500 mb-4"
     >
       <h2 className="text-2xl font-bold mb-4">
-        Bug Bash 4X times more efficiently
-        {t("bug_bash_4x_more_efficiently")}
+        {t('bug_bash_4x_more_efficiently')}
         {t}
       </h2>
-      <p>Start bug bashing with an Agile approach</p>
-      {t("start_bug_bashing_with_agile_approach")}
+      <p>{t('start_bug_bashing_with_agile_approach')}</p>
+
       <Link type="button" className="btn btn-outline mt-4">
-        Let,s get started
-        {t("lets_get_started")}
+        {t('lets_get_started')}
       </Link>
     </div>
   );
